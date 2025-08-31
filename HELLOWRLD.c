5047f35afc059de9181c965a7a415bdcc5cd3f74
Ryuzzen09 <yesamakshat@gmail.com>
2025-08-31 11:05:51 +0530

    Add files via upload

---
+++ HELLOWRLD.c
@@ -0,0 +1,15 @@
+/*Experiment 1: Installation, Environment Setup and starting with C language
+1.Write a program to print "Hello World"*/
+
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("Name - Akshat Singh Rawat\nSAP ID - 590028778\nCourse - BCA\nBatch - B6");
+    printf("\n--------------------------------------------");
+    printf("\nHello World");
+    printf("\n--------------------------------------------");
+    getch();
+    return 0;
+}
---
+++ exp 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("Name - Akshat Singh Rawat\nSAP ID - 590028778\nCourse - BCA\nBatch - B6");
+    printf("\n--------------------------------------------");
+    printf("\nStreet: 12 azad vihar Ajabpur Kala ");
+printf("\nCity: Dehradun");
+printf("\nState: Uttarakhand");
+printf("\nPincode: 248121");
+    printf("\n--------------------------------------------");
+    getch();
+    return 0;
+}
